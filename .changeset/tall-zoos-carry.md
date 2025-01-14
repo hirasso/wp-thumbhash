6af28d85147b6c4d930cd7af32d169d290e81c2a
Rasso Hilber <mail@rassohilber.com>
2025-01-14 08:23:36 +0100

    Remove tests that are already covered by `@hirasso/thumbhash-custom-element`

---
+++ .changeset/tall-zoos-carry.md
@@ -0,0 +1,5 @@
+---
+"wp-thumbhash": patch
+---
+
+Remove tests that are already covered by `@hirasso/thumbhash-custom-element`
--- tests/E2E/tests/frontend.test.ts
+++ tests/E2E/tests/frontend.test.ts
@@ -14,9 +14,6 @@ test.describe("Render", () => {
 
     const canvas = el.locator("thumb-hash canvas");
     expect(canvas).toHaveCount(1);
-    expect(canvas).toHaveAttribute("width");
-    expect(canvas).toHaveAttribute("height");
-    expect(canvas).toHaveAttribute("style", "width: 100%; height: 100%;");
   });
 
   test("supports explicit strategy 'canvas'", async ({ page }) => {
@@ -27,9 +24,6 @@ test.describe("Render", () => {
 
     const canvas = el.locator("thumb-hash canvas");
     expect(canvas).toHaveCount(1);
-    expect(canvas).toHaveAttribute("width");
-    expect(canvas).toHaveAttribute("height");
-    expect(canvas).toHaveAttribute("style", "width: 100%; height: 100%;");
   });
 
   test("supports strategy 'img'", async ({ page }) => {
@@ -39,8 +33,7 @@ test.describe("Render", () => {
     expect(el.locator("thumb-hash")).toHaveAttribute("strategy", "img");
 
     const img = el.locator("thumb-hash img");
-    expect(img).toHaveAttribute("style", "width: 100%; height: 100%;");
-    expect(img).toHaveAttribute("alt", "");
+    expect(img).toHaveCount(1);
   });
 
   test("supports strategy 'average'", async ({ page, browserName }) => {
@@ -54,9 +47,5 @@ test.describe("Render", () => {
 
     const div = el.locator("thumb-hash div");
     expect(div).toHaveCount(1);
-    expect(div).toHaveAttribute(
-      "style",
-      "width: 100%; height: 100%; background: rgb(163, 134, 104);",
-    );
   });
 });
